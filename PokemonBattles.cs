0f916f6ab9c554f098e72346c379b54eb6ccc4e6
ivo29 <concrete81@gmail.com>
2017-07-03 20:12:07 +0300

    adding Pokemon battles task and solution

---
+++ PokemonBattles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.PokemonBattles
+{
+    class PokemonBattles
+    {
+        static void Main(string[] args)
+        {
+            int pokemonCountFirst = int.Parse(Console.ReadLine());
+            int pokemonCountSecond = int.Parse(Console.ReadLine());
+            int maxBattles = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 1; i <= pokemonCountFirst; i++)
+            {
+                for (int j = 1; j <= pokemonCountSecond; j++)
+                {
+                    if (maxBattles <= counter)
+                    {
+                        break;
+                    }
+                    counter++;
+                    Console.Write("({0} <-> {1}) ", i, j);
+                    
+                }
+            }
+        }
+    }
+}
